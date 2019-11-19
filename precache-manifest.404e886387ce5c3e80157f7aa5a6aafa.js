18ec71686329d8c71975cef86314b63dcad1255b
Catharina <catharina.sorum@outlook.com>
2019-11-19 16:23:07 +0100

    Updates

---
+++ precache-manifest.404e886387ce5c3e80157f7aa5a6aafa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8e221a212cee084ea466c697297cb5",
+    "url": "/react_oppgavesett3/index.html"
+  },
+  {
+    "revision": "4b79cd5586405fac9488",
+    "url": "/react_oppgavesett3/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7094811ac53354f66753",
+    "url": "/react_oppgavesett3/static/js/2.41dab7da.chunk.js"
+  },
+  {
+    "revision": "4b79cd5586405fac9488",
+    "url": "/react_oppgavesett3/static/js/main.e6b5f130.chunk.js"
+  },
+  {
+    "revision": "1c8fe8c431a9f5f36d1f",
+    "url": "/react_oppgavesett3/static/js/runtime-main.e1db7fdc.js"
+  }
+]);
